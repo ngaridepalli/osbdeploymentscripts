fb93b80ccf06d4a7dbbefc10c731b839e184443c
ngaridepalli <ngaridepalli85@gmail.com>
2017-03-22 15:12:06 -0500

    added files

---
+++ deploy.cmd
@@ -0,0 +1,5 @@
+set FMW_HOME=C:\osb\Middleware
+set ANT_HOME=%FMW_HOME%\modules\org.apache.ant_1.7.1
+set PATH=%ANT_HOME%\bin;%PATH%
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_79
+set CLASSPATH=%FMW_HOME%/wlserver_10.3/server/lib/weblogic.jar;%FMW_HOME%/Oracle_OSB1/lib/alsb.jar;%FMW_HOME%/Oracle_OSB1/modules/com.bea.common.configfwk_1.7.0.0.jar
